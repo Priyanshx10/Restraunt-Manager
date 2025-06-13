a2a15ce7afb6e27fe0afc50df057a787c4e13dab
Priyansh <Priyanshyadav1012@gmail.com>
2025-06-13 09:29:48 +0530

    Task 01

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-yellow-400 to-red-500">
+      <h1 className="text-4xl font-bold text-white">
+        🍽️ Welcome to the Restaurant
+      </h1>
+    </div>
+  );
+}
+export default App;
